b6df08c2f23ade9f598d71209b30ff78fe72d5d5
geek <geek@localhost.localdomain>
2021-05-02 08:29:02 -0400

    Fixed paths

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,6 +4,6 @@ echo "+++++++++++++++++++++++++++++"
 echo "***** Testing the JAR *******"
 echo "+++++++++++++++++++++++++++++"
 
-WORKSPACE=/home/geek/jenkins/pipeline
+WORKSPACE=/home/geek/jenkins/jenkins_home/workspace/pipeline_docker_maven
 
 docker run --rm -v $WORKSPACE/simple-java-maven-app-original:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
